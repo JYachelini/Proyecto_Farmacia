060e5370121da42e17333b382cce9cb896bbd4c5
Julián Yachelini <juliyachelini@gmail.com>
2021-09-01 22:39:41 -0300

    fourteen commit

--- js/main.js
+++ js/main.js
@@ -52,6 +52,7 @@ let total = 0;
 const DOMcarrito = document.querySelector('#carrito');
 const DOMtotal = document.querySelector('#total');
 const DOMvaciar = document.querySelector('#btn-vaciar');
+const miLocalStorage = window.localStorage;
 
 
 function imprimirDatosMedicamento(n){
@@ -99,10 +100,19 @@ function imprimirDatosMedicamento(n){
 
         const buttonCarrito = document.createElement('button');
         buttonCarrito.classList.add('card-button');
-        buttonCarrito.innerText = `Añadir`;
+        // buttonCarrito.innerText = `Añadir`;
         buttonCarrito.setAttribute('id', 'add-animation');
         buttonCarrito.setAttribute('marcador', producto.id);
         buttonCarrito.addEventListener('click', addToCart);
+
+        buttonCarrito.innerHTML = `<svg id="cart" height="24" marcador="${producto.id}" viewBox="0 -31 512.00033 512" width="24" xmlns="http://www.w3.org/2000/svg"><path marcador="${producto.id}" d="m166 300.003906h271.003906c6.710938 0 12.597656-4.4375 14.414063-10.882812l60.003906-210.003906c1.289063-4.527344.40625-9.390626-2.433594-13.152344-2.84375-3.75-7.265625-5.964844-11.984375-5.964844h-365.632812l-10.722656-48.25c-1.523438-6.871094-7.617188-11.75-14.648438-11.75h-91c-8.289062 0-15 6.710938-15 15 0 8.292969 6.710938 15 15 15h78.960938l54.167968 243.75c-15.9375 6.929688-27.128906 22.792969-27.128906 41.253906 0 24.8125 20.1875 45 45 45h271.003906c8.292969 0 15-6.707031 15-15 0-8.289062-6.707031-15-15-15h-271.003906c-8.261719 0-15-6.722656-15-15s6.738281-15 15-15zm0 0"/><path marcador="${producto.id}" d="m151 405.003906c0 24.816406 20.1875 45 45.003906 45 24.8125 0 45-20.183594 45-45 0-24.8125-20.1875-45-45-45-24.816406 0-45.003906 20.1875-45.003906 45zm0 0"/><path marcador="${producto.id}" d="m362.003906 405.003906c0 24.816406 20.1875 45 45 45 24.816406 0 45-20.183594 45-45 0-24.8125-20.183594-45-45-45-24.8125 0-45 20.1875-45 45zm0 0"/></svg>`
+
+        const buttonCarritoTXT = document.createElement('p');
+        buttonCarritoTXT.setAttribute('id', 'button-carrito-txt');
+        buttonCarritoTXT.classList.add('button-carrito-txt');
+        buttonCarritoTXT.setAttribute('marcador', producto.id);
+        buttonCarritoTXT.innerText = 'Añadir';
+        buttonCarrito.appendChild(buttonCarritoTXT);
 
         linkImagen.appendChild(imagen);
         divCard.appendChild(linkImagen);
@@ -141,6 +151,8 @@ function addToCart(evt){
     calcularTotal();
     // Actualizamos el carrito
     imprimirCarrito();
+    // Local Storage
+    guardarCarritoLS()
 }
 
 
@@ -244,6 +256,8 @@ function borrarItemCarrito(evt){
     comprobarProductos();
     // Calculamos el nuevo total
     calcularTotal();
+    // LS
+    guardarCarritoLS()
 }
 
 // Función calcular el total precio
@@ -274,10 +288,23 @@ function vaciarCarrito(){
     calcularTotal();
 }
 
+function guardarCarritoLS(){
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    // miLocalStorage.setItem('counter', JSON.stringify(counter));
+}
+
+function cargarCarritoLS(){
+    if(miLocalStorage.getItem('carrito') !== null){
+        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+        // counter = JSON.parse(miLocalStorage.getItem('counter'));
+    }
+}
+
 DOMvaciar.addEventListener('click', vaciarCarrito);
-
+cargarCarritoLS();
 ordernadosPorPrecio();
-
+calcularTotal();
+imprimirCarrito();
 
 let navbarLeft = document.querySelector('#navbarLeft');
 navbarLeft = navbarLeft.clientWidth
@@ -286,21 +313,23 @@ let navbarRight = document.querySelector('#navbarRight');
 navbarRight.style.width = navbarLeft+"px";
 
 const counter = document.getElementById('counter');
-const counterAnimation = document.querySelectorAll('#add-animation')
+const counterAnimation = document.querySelectorAll('#add-animation');
+// Animación: https://codepen.io/dmngu9/pen/oNzrRKZ NO ME FUNCIONA :(
+
+
 for(const animation of counterAnimation){
     animation.addEventListener('click', event =>{
         navbar.style.top="0";
         const cl = counter.classList;
         const c = 'animated-counter';
-    
         counter.innerText = carrito.length;
         cl.remove(c, cl.contains(c));
         setTimeout(() =>
             counter.classList.add('animated-counter')
         , 1)
     })
-
 }
+counter.innerText = carrito.length;
 
 document.getElementById('btn-carrito').addEventListener('click', function(){
     document.getElementById('overlay').classList.add('is-visible');
@@ -319,3 +348,14 @@ document.getElementById('overlay').addEventListener('click', function(){
     document.getElementById('modalCarrito').classList.remove('is-visible');
     document.getElementById('body').classList.remove('noScroll');
 })
+
+$(document).keydown(function(event) {
+    if (event.keyCode == 27) {
+        $('#overlay').removeClass('is-visible');
+        $('#modalCarrito').removeClass('is-visible');
+        $('body').removeClass('noScroll');
+        // document.getElementById('overlay').classList.remove('is-visible');
+        // document.getElementById('modalCarrito').classList.remove('is-visible');
+        // document.getElementById('body').classList.remove('noScroll');
+    }
+});
